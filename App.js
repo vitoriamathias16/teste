e0fa0e79f1ce444363dee69840ad4f69dc92ff6b
vitoriamathias16 <vitoriamathias005@gmail.com>
2024-03-12 12:02:58 -0300

    botao

--- App.js
+++ App.js
@@ -1,5 +1,5 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Button } from 'react-native';
 
 export default function App() {
   return (
@@ -7,6 +7,7 @@ export default function App() {
       <Text>vitoria caroline mathias</Text>
       <Text>novo commit clone</Text>
       <Text>novo commit clone</Text>
+      <Button title='Salvar'></Button>
       <StatusBar style="auto" />
     </View>
   );
